28034265f2d3bbd7a511d5c003c029a1ffb1c766
Yer Yang <yyhmg_86@yahoo.com>
2023-09-22 10:41:58 -0700

    toggles between 'instock' and 'sold out' when a card button is clicked

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({ plant }) {
   const { id, name, image, price } = plant
+  const [inStock, setInStock] = useState(true)
+
+  function handleClick() {
+    setInStock((inStock) => !inStock)
+  }
+
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: ${price.toFixed(2)}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {inStock ? (
+        <button className="primary" onClick={handleClick}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleClick}>Out of Stock</button>
       )}
     </li>
   );
